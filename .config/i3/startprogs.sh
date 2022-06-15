d1ebe810f132e16401de9ed7bb0722c04ce78f67
ariedro <ariedro@gmail.com>
2022-06-15 12:00:03 -0300

    x11 update broke everything

--- .config/i3/startprogs.sh
+++ .config/i3/startprogs.sh
@@ -3,23 +3,23 @@
 notify-send "Loading"
 i3-msg 'workspace 1; exec /usr/bin/firefox'
 sleep 1
-i3-msg 'workspace 1; move workspace to output HDMI-1'
+i3-msg 'workspace 1; move workspace to output HDMI1'
 sleep 1
 i3-msg 'workspace 2; exec /usr/bin/alacritty'
 sleep 1
-i3-msg 'workspace 2; move workspace to output eDP-1'
+i3-msg 'workspace 2; move workspace to output eDP1'
 sleep 1
 i3-msg 'workspace 4; exec /usr/bin/emacs'
 sleep 1
-i3-msg 'workspace 4; move workspace to output HDMI-1'
+i3-msg 'workspace 4; move workspace to output HDMI1'
 sleep 1
 i3-msg 'workspace 8; exec /usr/bin/slack'
 sleep 2
-i3-msg 'workspace 8; move workspace to output eDP-1'
+i3-msg 'workspace 8; move workspace to output eDP1'
 sleep 1
 i3-msg 'workspace 9; exec /usr/bin/telegram-desktop'
 sleep 3
 i3-msg "workspace A; exec /usr/bin/firefox -new-window $MUSIC_URL"
 sleep 1
-i3-msg 'workspace A; move workspace to output eDP-1'
+i3-msg 'workspace A; move workspace to output eDP1'
 notify-send "Welcome"
